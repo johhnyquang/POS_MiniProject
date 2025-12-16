e0b342535d872efcceadbd568eb8af6a905398f3
Johhny Quang <146002224+johhnyquang@users.noreply.github.com>
2025-12-16 12:07:09 +0700

    Initial Commit Front_end và đổi port CORS FE trong Programs.cs

--- back_end/POS_Backend/POS_Backend/Program.cs
+++ back_end/POS_Backend/POS_Backend/Program.cs
@@ -21,7 +21,7 @@ namespace POS_Backend
             {
                 options.AddPolicy("AllowReactApp", policy =>
                 {
-                    policy.WithOrigins("http://localhost:3000")
+                    policy.WithOrigins("http://localhost:5173")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
